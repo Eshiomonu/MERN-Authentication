74459e6e44106f682da300b80000060d049af05e
kadirisunday <kadirisunday212@gmail.com>
2025-06-02 13:08:37 +0100

    Feat: controller function for user registration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,7 @@
+import bcrypt from "bcrypt";
+import { JsonWebTokenError } from "jsonwebtoken";
+import userModel from "../models/userModel.jss";
+
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -6,6 +10,26 @@ export const register = async (req, res) => {
   }
 
   try {
+    const existingUser = await userModel.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exist" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new userModel({ name, email, password: hashedPassword });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expireIn: "7D",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 3600 * 1000,
+    });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
