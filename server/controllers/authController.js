a64ef79aea394082257e499d70753743073ada2f
kadirisunday <kadirisunday212@gmail.com>
2025-06-02 12:12:03 +0100

    Added user model and user schema

---
+++ server/controllers/authController.js
@@ -0,0 +1,12 @@
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({ success: false, message: "Missing details" });
+  }
+
+  try {
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
